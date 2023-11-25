ca14e93c4561ec39a49b7f0475b34bf33db086a1
tatex74 <bruno.crespo74@gmail.com>
2023-11-25 19:40:03 +0100

    ajout fonction suppression base

--- base.c
+++ base.c
@@ -4,12 +4,12 @@
 #include "base.h"
 
 
-Base* create_base() {
+Base *create_base() {
     return (Base*) malloc(sizeof(Base));
 } 
 
 
-Base* add_rule_in_tail(Base *base, Rule *rule) {
+Base *add_rule_in_tail(Base *base, Rule *rule) {
     if (base == NULL) {
         base = rule;
     }
@@ -22,4 +22,20 @@ Base* add_rule_in_tail(Base *base, Rule *rule) {
     }
 
     return base;
+}
+
+
+void remove_base(Base *base) {
+    if (base != NULL) {
+        Rule *p = base;
+        Rule *tmp;
+        
+        while (p != NULL) {
+            tmp = p->next;
+            remove_rule(p);
+            p = tmp;
+        }   
+
+        free(base);
+    }
 }
