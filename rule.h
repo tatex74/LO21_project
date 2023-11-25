02d59d1c2784f564ac00a750034ee6a43264ad2f
iliass <135123856+narouiflix@users.noreply.github.com>
2023-11-25 20:01:21 +0100

    Update rule.h

--- rule.h
+++ rule.h
@@ -3,16 +3,18 @@
 
 #define PROPOSISTION_BUFFER 100
 
+// Structure représentant une proposition dans une règle
 typedef struct _Proposition {
     char proposition[PROPOSISTION_BUFFER];
     struct _Proposition *next;    
 } Proposition;
 
 
+// Structure représentant une règle dans la base de connaissances
 typedef struct _Rule {
-    Proposition *premise;
-    char conclusion[PROPOSISTION_BUFFER];
-    struct _Rule *next;
+    Proposition *premise; // Prémisse de la règle (liste de propositions)
+    char conclusion[PROPOSISTION_BUFFER]; // Conclusion de la règle
+    struct _Rule *next; // Pointeur vers la règle suivante dans la base de connaissances
 } Rule;
 
 Rule *create_rule();
